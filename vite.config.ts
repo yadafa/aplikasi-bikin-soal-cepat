7be506d1391baead9d1af4921be171699c2a5841
yadafa <39834098+yadafa@users.noreply.github.com>
2025-11-28 09:14:05 +0700

    Update vite.config.ts
    git add .
    git commit -m "fix entry file for Netlify deploy"
    git push

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/", // BIAR PATH BENAR DI NETLIFY
-});
+export default {
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+};
