15936dc14f46009176c45c65442d754af63436a9
yadafa <39834098+yadafa@users.noreply.github.com>
2025-11-28 09:34:04 +0700

    Delete src/app.jsx

--- src/app.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>🎓 Aplikasi Bikin Soal Cepat</h1>
-      <p>Selamat datang! Aplikasi sudah berhasil dijalankan 🎉</p>
-    </div>
-  );
-}
